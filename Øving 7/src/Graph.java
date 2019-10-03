923bc75deb94c4bded35e4f969d29cc4e5340999
maxts <max.torre.schau@gmail.com>
2019-10-03 13:46:20 +0200

    Finished øving 7

--- Øving 7/src/Graph.java
+++ Øving 7/src/Graph.java
@@ -6,6 +6,7 @@ public class Graph {
     int N;
     int E;
 
+
     Node[] node;
 
     public void ny_ugraf(BufferedReader br) throws IOException {
@@ -30,11 +31,11 @@ public class Graph {
     Give every node a distance of infinity
     The startnode gets distance of 0
      */
-    public void initLast(Node s) {
+    public void initLast(Node startnode) {
         for (int i = node.length; i-- > 0;) {
             node[i].d = new Last();
         }
-        ((Last)s.d).dist = 0;
+        ((Last)startnode.d).dist = 0;
     }
 
     /*
@@ -50,7 +51,50 @@ public class Graph {
         return res + " ";
     }
 
-    public void topolog
+    /*
+    A depth-first method which goes through a dept-first search for a specific node and returns the first node of the list once done
+     */
+    public Node df_topo(Node startnode, Node first) {
+        Topo_lst nd = (Topo_lst)startnode.d;
+        if (nd.visited) {
+            return first;
+        }
+        nd.visited = true;
+        for (Edge edge = startnode.edge1; edge != null; edge = edge.next) {
+            first = df_topo(edge.to, first);
+        }
+        nd.next = first;
+        return startnode;
+    }
+
+    public void printRes() {
+        Node n = topoligisort();
+        Topo_lst t = (Topo_lst) n.d;
+
+        while (n != null) {
+            System.out.print(findNode(n) + " ");
+            if (t.next != null) {
+                n = t.next;
+                t = (Topo_lst) n.d;
+            } else {
+                return;
+            }
+        }
+
+    }
+
+    public Node topoligisort() {
+        Node l = null;
+        for (int i = N; i-- > 0;) { //Initializes the depth-first-search
+            node[i].d = new Topo_lst();
+        }
+        for (int i = N; i -- > 0;) {
+            l = df_topo(node[i], l);
+        }
+        return l; //Returns the first element
+    }
+
+
 
     public void breadthFirstSearch(int i) {
         Node s = node[i];
@@ -60,10 +104,10 @@ public class Graph {
         while (!queue.empty()) {
             Node n = (Node)queue.nextInQueue(); //Gets the next node
             for (Edge e = n.edge1; e != null; e= e.next) {
-                Last l = (Last)e.to.d;
-                if(l.dist == l.infinity) {
-                    l.dist = ((Last)n.d).dist +1;
-                    l.last = n;
+                Last nextnode = (Last)e.to.d;
+                if(nextnode.dist == nextnode.infinity) {
+                    nextnode.dist = ((Last)n.d).dist +1;
+                    nextnode.last = n;
                     queue.add(e.to);
                 }
             }
--- Øving 7/src/Main.java
+++ Øving 7/src/Main.java
@@ -4,10 +4,17 @@ import java.io.FileReader;
 public class Main {
     public static void main(String[] args) {
         try {
+            //Oppgave 1:
             Graph g = new Graph();
-            g.ny_ugraf(new BufferedReader(new FileReader("test.txt")));
+            g.ny_ugraf(new BufferedReader(new FileReader("lg1.txt")));
             g.breadthFirstSearch(5);
             System.out.println(g.printGraph());
+
+            //Oppgave 2:
+
+            Graph g1 = new Graph();
+            g1.ny_ugraf(new BufferedReader(new FileReader("L7g5.txt")));
+            g1.printRes();
         }catch(Exception e) {
             e.printStackTrace();
         }
