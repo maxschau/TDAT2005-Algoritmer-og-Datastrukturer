006d7ff571cad3d0b792040c97b30a4491156d6b
maxts <max.torre.schau@gmail.com>
2019-11-11 21:15:15 +0100

    Fixed heap git add .!

--- Øving12/src/HuffmannTree.java
+++ Øving12/src/HuffmannTree.java
@@ -1,22 +1,73 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.*;
 
 public class HuffmannTree {
-    PriorityQueue<Node> priorityQueue;
+    //PriorityQueue<Node> priorityQueue;
+    int[] frequencies = new int[256];
+    ArrayList<Node> characters;
+
 
     public HuffmannTree() {
-        //The priorityQueue will work as a maxHeap
-        this.priorityQueue = new PriorityQueue<>(new SortByFrequency());
+        characters = new ArrayList<>();
     }
 
-    public void makePriQueue(ArrayList<Node> a) {
-        Collections.sort(a, new SortByFrequency());
-        for (Node n : a) {
-            priorityQueue.add(n);
+    public void createTree(File f) {
+        try {
+            FileReader fr = new FileReader(f);   //Creation of File Reader object
+            BufferedReader br = new BufferedReader(fr);  //Creation of BufferedReader object
+            int c = 0;
+            while ((c = br.read()) != -1)         //Read char by Char
+            {
+                char character = (char) c;          //converting integer to char
+                if ((int) c >= 0 && (int) c <= 256) {
+                    frequencies[(int) c]++;
+                }
+            }
+
+            //Adding the nodes to the arraylist
+            for (int i = 0; i < frequencies.length; i++) {
+                if (frequencies[i] != 0) {
+                    Node n = new Node((char) i , frequencies[i]);
+                    characters.add(n);
+                }
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public void printItOut() {
-        System.out.println(Arrays.toString(priorityQueue.toArray()));
+    public void fix_heap() {
+        Collections.sort(characters, new SortByFrequency());
+        while (characters.size() >= 2) {
+            System.out.println(characters.toString());
+            //If both nodes are not "parents":
+            Node parent;
+            if (characters.get(0).value == 0 && characters.get(1).value == 0) {
+                parent = new Node(characters.get(0).frequency + characters.get(1).frequency, characters.get(0), characters.get(1));
+            } else if (characters.get(0).value != 0 && characters.get(1).frequency != 0) {
+                parent = new Node(characters.get(0).value + characters.get(1).frequency, characters.get(0), characters.get(1));
+            } else if (characters.get(0).frequency != 0 && characters.get(1).value != 0) {
+                parent = new Node(characters.get(0).frequency + characters.get(1).value, characters.get(0), characters.get(1));
+            } else {
+                parent = new Node(characters.get(0).value + characters.get(1).value, characters.get(0), characters.get(1));
+            }
+            characters.remove(0);
+            characters.remove(0);
+            characters.add(parent);
+        }
+        System.out.println(characters.toString());
     }
+
+    public String printNodesSorted() {
+        Collections.sort(characters, new SortByFrequency());
+        String output = "";
+        for (Node n : characters) {
+            output += "Char: '" + n.character + "'    freq: " + n.frequency + "\n";
+        }
+        return output;
+    }
+
 
 }
--- Øving12/src/Node.java
+++ Øving12/src/Node.java
@@ -3,12 +3,27 @@ public class Node {
     char character;
     Node left;
     Node right;
+    int value;
 
     public Node(char character, int frequency) {
         this.frequency = frequency;
         this.character = character;
         this.left = null;
         this.right = null;
+    }
+
+    public Node(int value, Node left, Node right) {
+        this.value = value;
+        this.left = left;
+        this.right= right;
+    }
+
+    public boolean isParent(Node n) {
+        if (n.right != null || n.left != null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void incrementFrequency() {
@@ -25,7 +40,12 @@ public class Node {
 
     @Override
     public String toString() {
-        return "Char: " + character + " frequency: " +frequency;
+        if (character != 0) {
+            return "Char: " + character + " frequency: " +frequency;
+        } else {
+            return "Value: " + value;
+        }
+
     }
 }
 
